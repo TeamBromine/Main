bc8bbc86fa5264395eddb11365b7691b338fbb7b
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-10-19 02:32:52 +0300

    Added ComputerFactory.Common projects for contracts

---
+++ ComputerFactory/ComputerFactory.Common/Utils/RandomUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace ComputerFactory.Common.Utils
+{
+    using System;
+    using System.Linq;
+
+    public static class RandomUtils
+    {
+        private static readonly Random RandomGenerator = new Random();
+
+        public static int GenerateNumberInRange(int minValue, int maxValue) 
+        {
+            return RandomGenerator.Next(minValue, maxValue + 1);
+        }
+
+        public static string GenerateRandomString(int length)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[RandomGenerator.Next(s.Length)]).ToArray());
+        }
+    }
+}
